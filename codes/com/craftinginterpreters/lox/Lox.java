30803f8d385f019ea5b975a9fd818a7863bd1004
kaito23093 <cika23093@g.nihon-u.ac.jp>
2025-11-25 12:01:48 +0900

    Add Lox.java,and TokenType.java

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -47,7 +47,6 @@ public class Lox {
         if(hadError) System.exit(65);
     }
     
-    // --- エラー処理 ---
     
     static void error(int line, String message) {
         report(line, "", message);
